984fa3f644271ceffba4f91e34ad7a5244895722
Nicolas Threatt <nicolasthreattcu@gmail.com>
2023-06-01 14:26:56 -0400

    Logistic Regression Model Creation

--- supervised-learning/logistic-regression/win_probability.py
+++ supervised-learning/logistic-regression/win_probability.py
@@ -5,7 +5,6 @@ from sklearn.linear_model import LogisticRegression
 
 BASE_URL = "https://stats.nba.com/stats/"
 HEADERS = {
-    # HEADERS for the API request
     "Host": "stats.nba.com",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0",
     "Accept": "application/json, text/plain, */*",
@@ -18,6 +17,17 @@ HEADERS = {
     "Pragma": "no-cache",
     "Cache-Control": "no-cache",
 }
+
+FEATURES = [
+    "EVENTNUM",
+    "EVENTMSGTYPE",
+    "EVENTMSGACTIONTYPE",
+    "PERIOD",
+    "PCTIMESTRING",
+    "SCORE",
+    "SCOREMARGIN",
+]
+TARGET = "HOME_WIN"
 
 
 def load_games(season: str) -> dict:
@@ -121,11 +131,16 @@ def get_clutch_events(season: str) -> pd.DataFrame:
         # Append each clutch-time play to the DataFrame
         for row in pbp_data["resultSets"][0]["rowSet"]:
             gc_time_minutes = int(row[6].split(":")[0])
-            if row[10]:  # row[10] is the score
+            if row[10]:  # row[10] = score
                 home_vs_away_score = row[10].split(" - ")
                 score_diff = int(home_vs_away_score[0]) - int(home_vs_away_score[1])
                 if gc_time_minutes <= 5 and abs(score_diff) <= 5:  # Clutch-time
                     df.loc[len(df.index)] = row
+                    print()
+                    print(columns)
+                    print("Clutch-time play:", row)
+                    print()
+                    # break
 
     return df
 
@@ -139,23 +154,24 @@ def create_model(dfTrain: pd.DataFrame) -> LogisticRegression:
     Returns:
         sklearn.linear_model.LogisticRegression: A logistic regression model.
     """
-    FEATURES = [
-        "EVENTNUM",
-        "EVENTMSGTYPE",
-        "EVENTMSGACTIONTYPE",
-        "PERIOD",
-        "PCTIMESTRING",
-        "SCORE",
-        "SCOREMARGIN",
-    ]
-    TARGET = "HOME_WIN"
 
     X = dfTrain.loc[FEATURES].values
     y = dfTrain.loc[TARGET].values
 
-    model = LogisticRegression()
-    model.fit(X, y)
-    return model
+    return LogisticRegression().fit(X, y)
+
+
+def evaluate_model(df: pd.DataFrame, model: LogisticRegression) -> None:
+    """Evaluates a logistic regression model.
+
+    Args:
+        df (pd.DataFrame): Data to evaluate the model on.
+        model (sklearn.linear_model.LogisticRegression): The logistic regression model.
+    """
+    X = df.loc[FEATURES].values
+    y = df.loc[TARGET].values
+
+    print("\nAccuracy:", model.score(X, y))
 
 
 if __name__ == "__main__":
@@ -165,5 +181,19 @@ if __name__ == "__main__":
     print("\nGetting Test Data...")
     dfTest = get_clutch_events("2022-23")
 
-    print(dfTrain.head())
-    print(dfTest.head())
+    if not dfTrain or not dfTest:
+        exit("No data returned. Exiting...")
+
+    model = create_model(dfTrain)
+
+    dfPredict = (
+        dfTest.assign(
+            predicted_winner=model.predict(dfTest.loc[FEATURES].values),
+            home_win_prob=model.predict_proba(dfTest.loc[FEATURES].values)[:, 1],
+            # away_win_prob = model.predict_proba(dfTest.loc[FEATURES].values)[:,0])
+        )
+    )[dfTest.columns.tolist() + ["predicted_winner", "home_win_prob"]]
+
+    print("\nPredictions:")
+    print(dfPredict.head())
+    print(evaluate_model(dfTest, model))
