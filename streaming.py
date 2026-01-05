433a5f90e2fac52032e2bf8b2744c645a02622d7
Nicolas Threatt <nicolasthreattcu@gmail.com>
2026-01-04 19:53:58 -0600

    Streaming Interface Implementaion

--- streaming.py
+++ streaming.py
@@ -1,44 +1,29 @@
-# Description: This file is the main file for the streaming application.
-import argparse
 import time
-import threading
+
 from src.topics.producer import Producer
 from src.topics.consumer import Consumer
 
-
-def cli():
-    """Parses command line arguments."""
-    parser = argparse.ArgumentParser(description='Streaming application')
-    parser.add_argument('-id', '--game-id', type=str, required=True, help='Game ID')
-    parser.add_argument('-t', '--topic', type=str, required=True, help='Topic name')
-    args = parser.parse_args()
-
-    return args
+from cli import cli
 
 
-def main(game_id: str, out_topic: str):
-    """Main function for the streaming application.
-
-    Args:
-        game_id (str): The game ID.
-        out_topic (str): The name of the topic to write to.
-    """
+def main(game_id: str):
     
-    # Create threads of a producer and a consumer
-    tasks = [Producer(game_id), Consumer(out_topic)]
+    tasks = [Producer(game_id), Consumer(game_id)]
 
-    # Start threads of a producer and a consumer to 'topic_name' Kafka topic
-    for t in tasks:
-        t.start()
-
-    # Sleep for 10 seconds
-    time.sleep(10)
-
-    # Stop threads
     for task in tasks:
-        task.stop()
+        task.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    finally:
+        for task in tasks:
+            task.stop()
+        for task in tasks:
+            task.join()
+        print("Streaming stopped.")
 
 
 if __name__ == "__main__":
     args = cli()
-    main(args.game_id, args.topic)
+    main(args.game_id)
